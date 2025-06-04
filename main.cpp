9e4ab2dd93860f3ab1de2e081529c18adcfd9263
Otavio Santos <85749511+DEVOtavioSantos@users.noreply.github.com>
2025-06-04 16:19:57 -0300

    fthtejhtej

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <graphics.h>
+
+int main()
+{
+    initwindow(1024,576,"Teste",10,10);
+    int x = getmaxx(), y = getmaxy(), midy = getmaxy()/2 ;
+
+    outtextxy(x/2,midy -200, "Bem vindo ao Jogo do pato");
+
+
+
+
+
+    do
+    {
+        int xmouse = mousex(), ymouse = mousey();
+        circle(xmouse,ymouse, 10);
+    }while(!kbhit());
+}
