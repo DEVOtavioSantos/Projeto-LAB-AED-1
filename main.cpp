e80a5423f572714d772322cd6bb141221801b209
Otavio Santos <85749511+DEVOtavioSantos@users.noreply.github.com>
2025-06-04 17:45:40 -0300

    Commit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,31 @@
 #include <graphics.h>
-#include <conio.h>
-#include <iostream>
+
 
 int main() {
-    int gd = DETECT, gm;
-    initwindow(100,200,"Teste",10,10);
+    int MidX, Midy,Xmouse, ymouse;
+    initwindow(1024,576,"Teste",10,10);
 
-   
-    outtextxy(10, 50, (char*)"Teste");
-    
-    
-    getch(); // Aguarda tecla
+
+    MidX = getmaxx() /2 ;
+    Midy = getmaxy() /2;
+
+    outtextxy(MidX, Midy-200,"Acerte o pato");
+    do
+    {
+
+        Xmouse = mousex();
+        ymouse = mousey();
+
+
+
+
+
+    }while(!kbhit());
+
+
+
+    getch();
     closegraph();
+
     return 0;
 }
