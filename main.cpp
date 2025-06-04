3f54a1955b0d3f99e6477124efcfdda6e41a9c2a
Otavio Santos <85749511+DEVOtavioSantos@users.noreply.github.com>
2025-06-04 14:35:11 -0300

    lknhn

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <graphics.h>
+#include <conio.h>
+#include <iostream>
 
-int main()
-{
+int main() {
+    int gd = DETECT, gm;
     initwindow(100,200,"Teste",10,10);
-    getchar();
+
+   
+    outtextxy(10, 50, (char*)"Teste");
+    
+    
+    getch(); // Aguarda tecla
     closegraph();
     return 0;
 }
