42c00c639f27c57e887e65a44c152ff6792d66ca
Otavio Santos <85749511+DEVOtavioSantos@users.noreply.github.com>
2025-06-05 20:23:47 -0300

    Adicionado Coméntario

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 #include <graphics.h>
 #define tamanho 50
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 1600
+#define SCREEN_HEIGHT 900
 
 int main()
 {
-    initwindow(SCREEN_WIDTH,SCREEN_HEIGHT,"Tester",10,10);
-    int sizeX = getmaxx(), sizeY = getmaxy();
+    initwindow(SCREEN_WIDTH,SCREEN_HEIGHT,"Tester",10,10);//Cria uma janela
+    int sizeX = getmaxx(), sizeY = getmaxy();//Define variaveis referente ao tamanho da janela
 
 
 
@@ -24,18 +24,17 @@ int main()
         {
             getmouseclick(WM_LBUTTONDOWN, xmouse, ymouse);
             readimagefile("joao.jpg",xmouse-20,ymouse-15,xmouse+20,ymouse+20);
-            delay(200);
+            delay(100);
             cleardevice();
+
         }
 
 
 
-                setcolor(RED);
-
-
+        setcolor(RED);
         rectangle(sizeX/2,sizeY/2,(sizeX/2)+tamanho,(sizeY/2)+tamanho);
 
-
+    //aqui reconhece onde o mouse clicou
         if (xmouse > sizeX/2 && xmouse  < (sizeX/2)+tamanho)
         {
             if (ymouse > sizeY/2 && ymouse <(sizeY/2)+tamanho)
@@ -43,8 +42,15 @@ int main()
                 setcolor(RED);
                 outtextxy(sizeX/2, (sizeY/2)-200,"Acertou");
 
+
+
+
+
+
             }
+
         }
+
 
 
 
