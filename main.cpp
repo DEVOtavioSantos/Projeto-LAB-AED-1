d8c206094b477b150e0e4808cd40ccb6dc617c30
Otavio Santos <85749511+DEVOtavioSantos@users.noreply.github.com>
2025-06-05 20:38:05 -0300

    Coméntando

--- main.cpp
+++ main.cpp
@@ -9,17 +9,17 @@
 
 int main()
 {
-    initwindow(SCREEN_WIDTH,SCREEN_HEIGHT,"Tester",10,10);//Cria uma janela
+    initwindow(SCREEN_WIDTH,SCREEN_HEIGHT,"Tester");//Cria uma janela
     int sizeX = getmaxx(), sizeY = getmaxy();//Define variaveis referente ao tamanho da janela
 
 
 
-    do
+    do //Loop principal do jogo, onde faz que o jogo faz rodar ---Game Loop---
     {
-        int xmouse, ymouse;
+        int xmouse, ymouse;//variaveis da posição dos eixos X e Y do mouse
 
 
-
+        //reconhece onde o mouse clicar, e colocar um .JPG de uma mira, e apague logo após em 100 milísegundos
         while(ismouseclick(WM_LBUTTONDOWN))
         {
             getmouseclick(WM_LBUTTONDOWN, xmouse, ymouse);
@@ -34,22 +34,18 @@ int main()
         setcolor(RED);
         rectangle(sizeX/2,sizeY/2,(sizeX/2)+tamanho,(sizeY/2)+tamanho);
 
-    //aqui reconhece onde o mouse clicou
+        //aqui reconhece onde o mouse clicou
+
         if (xmouse > sizeX/2 && xmouse  < (sizeX/2)+tamanho)
         {
             if (ymouse > sizeY/2 && ymouse <(sizeY/2)+tamanho)
             {
                 setcolor(RED);
                 outtextxy(sizeX/2, (sizeY/2)-200,"Acertou");
-
-
-
-
-
-
             }
 
         }
+
 
 
 
